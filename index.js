a5450b1aa0c36b8710d94e19e2913afac30b63ad
deepikareddydasari <reddydeepikaa7@gmail.com>
2020-03-11 22:21:29 +0530

    2 files modified

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+//appDiv.innerHTML = `<h1>JS  Starter</h1>`;
+
+  function signup(){
+   var name = document.getElementById('name').value;
+   var pwd = document.getElementById('password').value;
+   var gen = document.getElementById('gender').value;
+   alert(name + " "+ pwd +" "+ gen)
+  }
+window.signup = signup;
